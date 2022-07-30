3292376e33fed7ddb2c41e0a51d35dfcb048ecf1
suraj sahu <int-SurajS@Netlink.com>
2022-07-31 01:17:08 +0530

    user api update

--- src/main/java/com/suraj/blogs/entities/User.java
+++ src/main/java/com/suraj/blogs/entities/User.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -25,14 +30,26 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+	@NotEmpty
+	@Size(min =4, message ="user name must be minimum of 4 characters")
 	private String name;
+	
+	@Email(message = "your given email address is not valid")
 	private String email;
+	
+	@NotEmpty
+	@Size(min = 8, max =16 ,message = "password must not be empty and must be between 8 And 16 characters long")
+	@Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})")
 	private String password;
+	
+	@NotEmpty(message = "address must not be blank")
 	private String address;
 	private String alternateAddress;
 	private String city;
 	private String state;
 	private int zip;
+	
+	@NotNull
 	private Long phone;
 	
 }
--- src/main/java/com/suraj/blogs/services/impl/UserServiceImpl.java
+++ src/main/java/com/suraj/blogs/services/impl/UserServiceImpl.java
@@ -1,20 +1,29 @@
 package com.suraj.blogs.services.impl;
 
-import java.util.List;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.suraj.blogs.entities.User;
 import com.suraj.blogs.payloads.UserDto;
 import com.suraj.blogs.repositories.UserRepo;
 import com.suraj.blogs.services.UserService;
+import com.suraj.blogs.exceptions.*;
 
+@Service
 public class UserServiceImpl implements UserService{
   
 	
 	@Autowired
 	private UserRepo userRepo;
 	
+	
+	@Autowired
+	private ModelMapper modelMapper;
 	
 	@Override
 	public UserDto createUser(UserDto userDto) {
@@ -27,59 +36,75 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public UserDto updateUser(UserDto user, Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto updateUser(UserDto userDto, Integer userId) {
+		User user = this.userRepo.findById(userId)
+				.orElseThrow(()-> new ResourceNotFoundException("User","Id",userId));
+		
+		user.setName(userDto.getName());
+		user.setAddress(userDto.getAddress());
+		user.setAlternateAddress(userDto.getAlternateAddress());
+		user.setState(userDto.getState());
+		user.setZip(userDto.getZip());
+		user.setCity(userDto.getCity());
+		User updatedUser= this.userRepo.save(user);
+		return this.userToUserDto(updatedUser);
 	}
 
 	@Override
 	public UserDto getUserById(Integer userId) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("user", "Id", userId));
+		return this.userToUserDto(user);
+
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> users= this.userRepo.findAll();	
+	    List<UserDto> userDto =	users.stream().map(user -> this.userToUserDto(user)).collect(Collectors.toList());
+		return userDto;
 	}
 
 	@Override
 	public void deleteUser(Integer userId) {
-		// TODO Auto-generated method stub
 		
+		User user=this.userRepo.findById(userId).orElseThrow(()-> new ResourceNotFoundException("user","id",userId));
+		this.userRepo.deleteById(userId);
 	}
 	
 	
 	public User dtoToUser(UserDto userDto) {
-		User user = new User();
-		user.setId(userDto.getId());
-		user.setName(userDto.getName());
-		user.setEmail(userDto.getEmail());
-		user.setPassword(userDto.getPassword());
-		user.setAddress(userDto.getAddress());
-		user.setAlternateAddress(userDto.getAlternateAddress());
-		user.setState(userDto.getState());
-		user.setCity(userDto.getCity());
-		user.setZip(userDto.getZip());
-		user.setPhone(user.getPhone());
+		User user = this.modelMapper.map(userDto, User.class);
+		
+//		User user = new User();
+//		user.setId(userDto.getId());
+//		user.setName(userDto.getName());
+//		user.setEmail(userDto.getEmail());
+//		user.setPassword(userDto.getPassword());
+//		user.setAddress(userDto.getAddress());
+//		user.setAlternateAddress(userDto.getAlternateAddress());
+//		user.setState(userDto.getState());
+//		user.setCity(userDto.getCity());
+//		user.setZip(userDto.getZip());
+//		user.setPhone(user.getPhone());
 		return user;
 	}
 
 	
 	public UserDto userToUserDto(User user) {
-		UserDto userDto = new UserDto();
 		
-		userDto.setId(user.getId());
-		userDto.setName(user.getName());
-		userDto.setEmail(user.getEmail());
-		userDto.setPassword(user.getPassword());
-		userDto.setAddress(user.getAddress());
-		userDto.setAlternateAddress(user.getAlternateAddress());
-		userDto.setState(user.getState());
-		userDto.setCity(user.getCity());
-		userDto.setZip(user.getZip());
-		userDto.setPhone(user.getPhone());
+		UserDto userDto = this.modelMapper.map(user, UserDto.class);
+		
+//		UserDto userDto = new UserDto();		
+//		userDto.setId(user.getId());
+//		userDto.setName(user.getName());
+//		userDto.setEmail(user.getEmail());
+//		userDto.setPassword(user.getPassword());
+//		userDto.setAddress(user.getAddress());
+//		userDto.setAlternateAddress(user.getAlternateAddress());
+//		userDto.setState(user.getState());
+//		userDto.setCity(user.getCity());
+//		userDto.setZip(user.getZip());
+//		userDto.setPhone(user.getPhone());
 		
 		
 		return userDto;
