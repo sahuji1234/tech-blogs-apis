8f71b45d7138ec3ecd32afa6e30c4e30a88cc3dd
suraj sahu <int-SurajS@Netlink.com>
2022-07-31 16:21:56 +0530

    post entity commit

---
+++ src/main/java/com/suraj/blogs/entities/Post.java
@@ -0,0 +1,46 @@
+package com.suraj.blogs.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="posts")
+@Getter
+@Setter
+public class Post {
+    
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer postId;
+	
+	private String title;
+	
+	
+	@Column(length = 1000)
+	private String content;
+	
+	private String imageName;
+	
+	private Date addedDate;
+	
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
+	
+	@ManyToOne
+	private User user;
+	
+	
+	
+}
--- src/main/java/com/suraj/blogs/entities/User.java
+++ src/main/java/com/suraj/blogs/entities/User.java
@@ -1,9 +1,15 @@
 package com.suraj.blogs.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
@@ -51,5 +57,8 @@ public class User {
 	
 	@NotNull
 	private Long phone;
+	
+	@OneToMany(mappedBy = "user" , cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 	
 }
